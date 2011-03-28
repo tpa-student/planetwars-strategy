c8713c5034bd6294067c8e980d1ae0e405abf6f5
Pawel Chwala <pawel.chwala@gmail.com>
2011-03-28 20:03:02 +0200

    Tuz przed wyjsciem ;)

--- src/test/java/com/tieto/planetwars/player/srategy/DescriptionKiCStrategy.java
+++ src/test/java/com/tieto/planetwars/player/srategy/DescriptionKiCStrategy.java
@@ -1,6 +1,5 @@
 package com.tieto.planetwars.player.srategy;
 
-import static org.mockito.Mockito.never;
 import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
 
@@ -16,27 +15,27 @@ import com.tieto.planetwars.world.WarsMap;
 
 public class DescriptionKiCStrategy {
 	KiCStrategy strategy = new KiCStrategy();
-	
+
 	@Test
 	public void should_have_method_doTurn() throws Exception {
 		WarsMap warsMap = Mockito.mock(WarsMap.class);
 		Player player = Mockito.mock(Player.class);
 		strategy.doTurn(warsMap, player);
 	}
-	
+
 	@Test
 	public void doTurn_should_check_planets_in_world() throws Exception {
 		// given
 		WarsMap warsMap = Mockito.mock(WarsMap.class);
 		List<Planet> planets = new ArrayList<Planet>();
 		when(warsMap.getPlanets()).thenReturn(planets);
-		
+
 		// when
 		strategy.doTurn(warsMap, null);
-		
+
 		// then
 		verify(warsMap).getPlanets();
-		
+
 	}
-	
+
 }
